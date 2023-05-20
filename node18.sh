3235385813eff6337e367d95ddf351ef6b78494c
HuanLinMaster <linsmc@126.com>
2023-05-20 18:32:26 +0800

    fix: node18

--- node18.sh
+++ node18.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 # 下载node-v18.16.0-linux-x64.tar.xz到/tmp目录
-
-echo "Downloading Node.js"
 wget -P /tmp https://npmmirror.com/mirrors/node/v18.16.0/node-v18.16.0-linux-x64.tar.xz
 # 解压到/opt/node-v18.16.0/目录
-echo "Unpacking Node.js to /opt/"
 tar -xvf /tmp/node-v18.16.0-linux-x64.tar.xz -C /opt/
-# 向~内的所有rc文件写入path /opt/node-v18.16.0/bin/
-read -p "Do you want to append path /opt/node-v18.16.0/bin/ to all bash configuration in your home directory? (y/n " answer
+# 询问用户是否要向~内的所有以点开头的rc文件写入path /opt/node-v18.16.0/bin/
+read -p "Do you want to append path /opt/node-v18.16.0/bin/ to all rc files starting with a dot in your home directory? (y/n) " answer
 if [ "$answer" == "y" ]; then
-  # 向~内的所有rc文件写入path /opt/node-v18.16.0/bin/
-  echo "OK"
-  for file in $(find ~ -name "*rc"); do
+  # 向~内的所有以点开头的rc文件写入path /opt/node-v18.16.0/bin/
+  for file in $(find ~ -maxdepth 1 -name ".*rc"); do
     echo "export PATH=/opt/node-v18.16.0/bin/:\$PATH" >> $file
   done
 fi
