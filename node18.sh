7223bd0144981ba6d720032f4523821b7990a651
HuanLinMaster <linsmc@126.com>
2023-05-20 18:34:18 +0800

    fix: unpack to opt

--- node18.sh
+++ node18.sh
@@ -3,11 +3,11 @@
 wget -P /tmp https://npmmirror.com/mirrors/node/v18.16.0/node-v18.16.0-linux-x64.tar.xz
 # 解压到/opt/node-v18.16.0/目录
 tar -xvf /tmp/node-v18.16.0-linux-x64.tar.xz -C /opt/
-# 询问用户是否要向~内的所有以点开头的rc文件写入path /opt/node-v18.16.0/bin/
-read -p "Do you want to append path /opt/node-v18.16.0/bin/ to all rc files starting with a dot in your home directory? (y/n) " answer
+# 询问用户是否要向~内的所有以点开头的rc文件写入path /opt/node-v18.16.0-linux-x64/bin/
+read -p "Do you want to append path /opt/node-v18.16.0-linux-x64/bin/ to all rc files starting with a dot in your home directory? (y/n) " answer
 if [ "$answer" == "y" ]; then
-  # 向~内的所有以点开头的rc文件写入path /opt/node-v18.16.0/bin/
+  # 向~内的所有以点开头的rc文件写入path /opt/node-v18.16.0-linux-x64/bin/
   for file in $(find ~ -maxdepth 1 -name ".*rc"); do
-    echo "export PATH=/opt/node-v18.16.0/bin/:\$PATH" >> $file
+    echo "export PATH=/opt/node-v18.16.0-linux-x64/bin/:\$PATH" >> $file
   done
 fi
