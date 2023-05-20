51b7cbf18a6c1ec341b20d20484de0a69aa8ad17
HuanLinMaster <linsmc@126.com>
2023-05-20 18:34:43 +0800

    fix: read

--- node18.sh
+++ node18.sh
@@ -4,7 +4,7 @@ wget -P /tmp https://npmmirror.com/mirrors/node/v18.16.0/node-v18.16.0-linux-x64
 # 解压到/opt/node-v18.16.0/目录
 tar -xvf /tmp/node-v18.16.0-linux-x64.tar.xz -C /opt/
 # 询问用户是否要向~内的所有以点开头的rc文件写入path /opt/node-v18.16.0-linux-x64/bin/
-read -p "Do you want to append path /opt/node-v18.16.0-linux-x64/bin/ to all rc files starting with a dot in your home directory? (y/n) " answer
+read -p "Do you want to append path /opt/node-v18.16.0-linux-x64/bin/ to all rc files starting with a dot in your home directory? (y/n) " answer;
 if [ "$answer" == "y" ]; then
   # 向~内的所有以点开头的rc文件写入path /opt/node-v18.16.0-linux-x64/bin/
   for file in $(find ~ -maxdepth 1 -name ".*rc"); do
